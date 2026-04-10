846672fe0c3beca8e6c513bc7c86fc12426733b5
luisca2006 <lg.castroa1@uniandes.edu.co>
2026-04-10 17:33:16 -0500

    ci

---
+++ dist/data.js
@@ -0,0 +1,59 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SERIES = void 0;
+exports.SERIES = [
+    {
+        id: 1,
+        name: 'Breaking Bad',
+        channel: 'AMC',
+        seasons: 5,
+        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/Breaking_Bad_Season_3.jpg/220px-Breaking_Bad_Season_3.jpg',
+        description: 'Set and filmed in Albuquerque, New Mexico, the series tells the story of Walter White, a struggling and depressed high school chemistry teacher who is diagnosed with lung cancer.',
+        url: 'https://www.amc.com/shows/breaking-bad'
+    },
+    {
+        id: 2,
+        name: 'Orange Is the New Black',
+        channel: 'Netflix',
+        seasons: 6,
+        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/86/Orange_Is_the_New_Black_Season_1.jpeg/220px-Orange_Is_the_New_Black_Season_1.jpeg',
+        description: 'Piper Chapman, a woman sentenced to 15 months in prison, struggles to adapt to life behind bars at Litchfield Penitentiary.',
+        url: 'https://www.netflix.com/title/70242311'
+    },
+    {
+        id: 3,
+        name: 'Game of Thrones',
+        channel: 'HBO',
+        seasons: 7,
+        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/d8/Game_of_Thrones_Season_3.jpg/220px-Game_of_Thrones_Season_3.jpg',
+        description: 'Nine noble families fight for control over the mythical lands of Westeros, while an ancient enemy returns after being dormant for thousands of years.',
+        url: 'https://www.hbo.com/game-of-thrones'
+    },
+    {
+        id: 4,
+        name: 'The Big Bang Theory',
+        channel: 'CBS',
+        seasons: 12,
+        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/dd/The_Big_Bang_Theory_Season_9_DVD.jpg/220px-The_Big_Bang_Theory_Season_9_DVD.jpg',
+        description: 'A woman who moves in next door to two brilliant but socially awkward physicists shows them how little they know about life outside of the laboratory.',
+        url: 'https://www.cbs.com/shows/big_bang_theory/'
+    },
+    {
+        id: 5,
+        name: 'Sherlock',
+        channel: 'BBC',
+        seasons: 4,
+        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/3/34/Sherlock_Title_Card.jpg/220px-Sherlock_Title_Card.jpg',
+        description: 'Sherlock depicts consulting detective Sherlock Holmes (Benedict Cumberbatch) solving various mysteries in modern-day London. Holmes is assisted by his flatmate and friend, Dr Watson.',
+        url: 'https://www.bbc.co.uk/programmes/b018ttws'
+    },
+    {
+        id: 6,
+        name: 'A Very English Scandal',
+        channel: 'BBC',
+        seasons: 2,
+        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/A_Very_English_Scandal.jpg/220px-A_Very_English_Scandal.jpg',
+        description: 'Based on true events, the story of British politician Jeremy Thorpe and Norman Scott, a former model who claimed to have had a relationship with him.',
+        url: 'https://www.bbc.co.uk/programmes/p065t3j6'
+    }
+];
---
+++ dist/serie.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Serie = void 0;
+class Serie {
+    constructor(id, name, channel, seasons, image, description, url) {
+        this.id = id;
+        this.name = name;
+        this.channel = channel;
+        this.seasons = seasons;
+        this.image = image;
+        this.description = description;
+        this.url = url;
+    }
+}
+exports.Serie = Serie;
