ec882d0dca16583a6f1ef0c420f50e15e29f198b
luisca2006 <lg.castroa1@uniandes.edu.co>
2026-04-10 17:28:45 -0500

    nada

--- src/data.ts
+++ src/data.ts
@@ -1,3 +1,31 @@
+export class Serie {
+  id: number;
+  name: string;
+  channel: string;
+  seasons: number;
+  image: string;
+  description: string;
+  url: string;
+
+  constructor(
+    id: number,
+    name: string,
+    channel: string,
+    seasons: number,
+    image: string,
+    description: string,
+    url: string
+  ) {
+    this.id = id;
+    this.name = name;
+    this.channel = channel;
+    this.seasons = seasons;
+    this.image = image;
+    this.description = description;
+    this.url = url;
+  }
+}
+
 import { Serie } from './serie';
 
 export const SERIES: Serie[] = [
